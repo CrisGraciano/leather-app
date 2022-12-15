6900f711c17a440a14924564f3cc34f0328d053b
CrisGraciano <cristobalgraciano98@gmail.com>
2022-12-14 21:03:51 -0600

    commented out apollo server(not using)

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-});
+// const server = new ApolloServer({
+// //   typeDefs,
+// //   resolvers,
+// });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
