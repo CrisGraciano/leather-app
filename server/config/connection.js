25c946c8c30b8f15aaa7ba4f1253d12b063b236a
Priscilla <edpris.montalvo@gmail.com>
2022-12-14 22:41:55 -0600

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+console.log(process.env.MONGODB_URI || 'mongodb://localhost:27017/leather-store');
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost:27017/leather-store',
     {
