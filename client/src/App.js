40495b1719322e8e05ebf32781b4a1fb9a9e4ba6
Priscilla <edpris.montalvo@gmail.com>
2022-12-19 12:27:41 -0600

    import app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
